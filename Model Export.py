79cb0a45e884ee5f3fe0175c53ab66654b3b4b2e
mirza1992 <98799016+mirza1992@users.noreply.github.com>
2022-06-02 17:32:52 +0500

    Create Model Export.py

---
+++ Model Export.py
@@ -0,0 +1,10 @@
+import pickle
+
+Pkl_Vectorizer = pickle.load(open("MirzaFiles/Pickle_TFIDF_Topic_Model.pkl","rb"))
+Pkl_LDA = pickle.load(open("MirzaFiles/Pickle_Topic_Model.pkl","rb"))
+
+
+for index, topic in enumerate(Pickle_Topic_Model.components_):
+  print(f"The Top Words For Topic #{index}")
+  print([Pickle_Topic_Models.get_feature_names()[i] for i in topic.argsort()[-15:]])
+  print('\n')
