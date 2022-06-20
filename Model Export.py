40ee5437a6e366a9bc3c7aaedff33738340c8fa1
mirza1992 <98799016+mirza1992@users.noreply.github.com>
2022-06-20 15:35:08 +0500

    update model code

--- Model Export.py
+++ Model Export.py
@@ -1,69 +1,68 @@
-from __future__ import print_function
-
-import pandas as pd
-from wordcloud import WordCloud
-import matplotlib.pyplot as plt
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.decomposition import LatentDirichletAllocation
-from nltk.tokenize import word_tokenize
-from nltk.corpus import stopwords
-import nltk
-import random
-import re
-import time
-import string
-nltk.download('stopwords')
-nltk.download('punkt')
 import pickle
-import pyLDAvis
-import pyLDAvis.sklearn
+import gensim
+import spacy
+import numpy as np
+import pandas as pd
+nlp = spacy.load("en_core_web_sm")
+
+Pkl_Vectorizer    = pickle.load(open("Pickle_TFIDF_Topic_Model.pkl","rb"))
+Pkl_best_LDAa     = pickle.load(open("Pickle_lda_best_Topic_Model.pkl","rb"))
+Pkl_keywords_LDAa = pickle.load(open("Pickle_keyword_Topic_Model.pkl","rb"))
+Pkl_fit_LDAa      = pickle.load(open("Pickle_lda_fit_Topic_Model.pkl","rb"))
 
 
-def content_input(content):
-    content = input("Enter Article:")
+mytext = []
+
+user_input = input("Enter Article:")
+
+mytext.append(user_input)
+
+
+def predict_topic(mytext):
+
+    def sent_to_words(sentences):
+        for sentence in sentences: 
+            yield(gensim.utils.simple_preprocess(str(sentence), deacc=True))  # deacc=True removes punctuations
+
+    def lemmatization(texts, allowed_postags=['NOUN', 'ADJ', 'VERB', 'ADV']):
+        """https://spacy.io/api/annotation"""
+        texts_out = []
+        for sent in texts:
+            doc = nlp(" ".join(sent))
+            texts_out.append(" ".join([token.lemma_ if token.lemma_ not in ['-PRON-'] else '' for token in doc if token.pos_ in allowed_postags]))
+        return texts_out        
+
+    mytext_2 = list(sent_to_words(mytext))
+
+    mytext_3 = lemmatization(texts = mytext_2, allowed_postags=['NOUN', 'ADJ', 'VERB', 'ADV'])
+
+    mytext_4 = Pkl_Vectorizer.transform(mytext_3)
+
+
+    def show_topics(vectorizer=Pkl_Vectorizer, lda_model=Pkl_fit_LDAa, n_words=20):
+        keywords = np.array(vectorizer.get_feature_names())
+        topic_keywords = []
+        for topic_weights in lda_model.components_:
+            top_keyword_locs = (-topic_weights).argsort()[:n_words]
+            topic_keywords.append(keywords.take(top_keyword_locs))
+        return topic_keywords
+
+    topic_keywords = show_topics(vectorizer=Pkl_Vectorizer, lda_model=Pkl_best_LDAa, n_words=15)
+
+    # Topic - Keywords Dataframe
+    df_topic_keywords = pd.DataFrame(topic_keywords)
+    df_topic_keywords.columns = ['Word '+str(i) for i in range(df_topic_keywords.shape[1])]
+    df_topic_keywords.index = ['Topic '+str(i) for i in range(df_topic_keywords.shape[0])]
+
+
+    topic_probability_scores = Pkl_best_LDAa.transform(mytext_4)
+    topic = df_topic_keywords.iloc[np.argmax(topic_probability_scores), :].values.tolist()
+
     
+    return print("\n"),print("Topics Predicted : ",topic,"\n"),print("Topics Allocated : ",np.argmax(topic_probability_scores))
 
-    def pre_processor(col_name):
-
-      col_name = col_name.replace('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', ' ')
-
-      # 1- Converting to Lower & Clear the Oppostrophy
-      col_name = col_name.lower().replace(u'\n', u' ').replace(u"’", u'').replace(u'“', u'').replace(u'”', u'').replace(u'   ', u'')
-
-      # 2- Remove punctuations
-      col_name = col_name.replace('[{}]'.format(string.punctuation), '')
-
-      # 4- Remove URL's
-      col_name = col_name.replace('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', ' ')
-
-
-      return col_name
-
-    content_preprocess = pre_processor(content)
-    pre_proc = [content_preprocess]
-
-
-    tfidf = TfidfVectorizer(stop_words='english',ngram_range=(2,2) ,strip_accents=None, lowercase=True, max_features=None, vocabulary=None)
-    tfidf.fit(pre_proc)
-    vect_tfidf = tfidf.fit_transform(pre_proc)
-
-
-    LDA_tfidf = LatentDirichletAllocation(n_components=10,           # Number of topics
-                                          max_iter=5,               # Max learning iterations
-                                          learning_method='online'          
-                                         )
-    LDA_tfidf.fit(vect_tfidf)
-
-
-
-    for index, topic in enumerate(LDA_tfidf.components_):
-      print('\n')
-      print(f"The Top Words For Topic #{index}")
-      print([tfidf.get_feature_names()[i] for i in topic.argsort()[-15:]])
-      print('\n')
-    pyLDAvis.enable_notebook()
-    pyLDAvis.sklearn.prepare(LDA_tfidf, vect_tfidf, tfidf)
-
-
+# import time
 if __name__ == "__main__":
-    content_input(content)
+    predict_topic(mytext)
+
+
